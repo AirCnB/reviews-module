5efadb3b2284369fa2d2f3eba044823430042589
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-07-24 12:38:20 -0700

    Add temp stars image

--- client/src/components/Stars.jsx
+++ client/src/components/Stars.jsx
@@ -13,49 +13,49 @@ class Stars extends React.Component {
 		if (this.props.rating !== 6) {
 			return (
 				<div className="search">
-					<img src="star.gif" alt="Star"/>
+					<img src="fivestars.png" alt="Star"/>
 				</div>
 			)
 		} else if (this.props.rating === 0) {
 			return (
 				<div className="search">
-					<img src="star.gif" alt="Star"/>
+					<img src="fivestars.png" alt="Star"/>
 				</div>
 			)
 		} else if (this.props.rating === 1) {
 			return (
 				<div className="search">
-					<img src="star.gif" alt="Star"/>
+					<img src="fivestars.png" alt="Star"/>
 				</div>
 			)
 		}else if (this.props.rating === 2) {
 			return (
 				<div className="search">
-					<img src="star.gif" alt="Star"/>
+					<img src="fivestars.png" alt="Star"/>
 				</div>
 			)
 		} else if (this.props.rating === 3) {
 			return (
 				<div className="search">
-					<img src="star.gif" alt="Star"/>
+					<img src="fivestars.png" alt="Star"/>
 				</div>
 			)
 		} else if (this.props.rating === 4) {
 			return (
 				<div className="search">
-					<img src="star.gif" alt="Star"/>
+					<img src="fivestars.png" alt="Star"/>
 				</div>
 			)
 		} else if (this.props.rating === 5) {
 			return (
 				<div className="search">
-					<img src="star.gif" alt="Star"/>
+					<img src="fivestars.png" alt="Star"/>
 				</div>
 			)	
 		} else {	
 			return (
 					<div className="search">
-						<img src="star.gif" alt="no stars"/>
+						<img src="fivestars.png" alt="no stars"/>
 					</div>
 				)
 		}
