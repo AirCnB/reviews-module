76a22fab6c7c52405e637d97f8bddef56e5550c4
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-07-24 12:50:32 -0700

    git

--- client/src/components/Stars.jsx
+++ client/src/components/Stars.jsx
@@ -13,50 +13,82 @@ class Stars extends React.Component {
 		if (this.props.rating !== 6) {
 			return (
 				<div className="search">
+<<<<<<< HEAD
 					<img src="fivestars.png" alt="Star"/>
+=======
+					<img src="star.gif" alt="Star"/>
+>>>>>>> 0efc78e33f2b0ba812dfd7bca6ffcc7861067237
 				</div>
 			)
 		} else if (this.props.rating === 0) {
 			return (
 				<div className="search">
+<<<<<<< HEAD
 					<img src="fivestars.png" alt="Star"/>
+=======
+					<img src="star.gif" alt="Star"/>
+>>>>>>> 0efc78e33f2b0ba812dfd7bca6ffcc7861067237
 				</div>
 			)
 		} else if (this.props.rating === 1) {
 			return (
 				<div className="search">
+<<<<<<< HEAD
 					<img src="fivestars.png" alt="Star"/>
+=======
+					<img src="star.gif" alt="Star"/>
+>>>>>>> 0efc78e33f2b0ba812dfd7bca6ffcc7861067237
 				</div>
 			)
 		}else if (this.props.rating === 2) {
 			return (
 				<div className="search">
+<<<<<<< HEAD
 					<img src="fivestars.png" alt="Star"/>
+=======
+					<img src="star.gif" alt="Star"/>
+>>>>>>> 0efc78e33f2b0ba812dfd7bca6ffcc7861067237
 				</div>
 			)
 		} else if (this.props.rating === 3) {
 			return (
 				<div className="search">
+<<<<<<< HEAD
 					<img src="fivestars.png" alt="Star"/>
+=======
+					<img src="star.gif" alt="Star"/>
+>>>>>>> 0efc78e33f2b0ba812dfd7bca6ffcc7861067237
 				</div>
 			)
 		} else if (this.props.rating === 4) {
 			return (
 				<div className="search">
+<<<<<<< HEAD
 					<img src="fivestars.png" alt="Star"/>
+=======
+					<img src="star.gif" alt="Star"/>
+>>>>>>> 0efc78e33f2b0ba812dfd7bca6ffcc7861067237
 				</div>
 			)
 		} else if (this.props.rating === 5) {
 			return (
 				<div className="search">
+<<<<<<< HEAD
 					<img src="fivestars.png" alt="Star"/>
+=======
+					<img src="star.gif" alt="Star"/>
+>>>>>>> 0efc78e33f2b0ba812dfd7bca6ffcc7861067237
 				</div>
 			)	
 		} else {	
 			return (
 					<div className="search">
+<<<<<<< HEAD
 						<img src="fivestars.png" alt="no stars"/>
 
+=======
+						<img src="star.gif" alt="no stars"/>
+>>>>>>> 0efc78e33f2b0ba812dfd7bca6ffcc7861067237
 					</div>
 				)
 		}
