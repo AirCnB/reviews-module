23ec8c27503b05d71d3ccd6303c7449e0f03987e
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-07-23 19:38:27 -0700

    Add Other Components

---
+++ client/src/components/Flag.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Flag extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			
+		}
+	}
+
+	render () {
+		return (
+			<div className="flag">
+
+			</div>
+		)
+	}
+}
+
+export default Flag;
---
+++ client/src/components/PageTabs.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class PageTabs extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			
+		}
+	}
+
+	render () {
+		return (
+			<div className="pagetabs">
+
+			</div>
+		)
+	}
+}
+
+export default PageTabs;
