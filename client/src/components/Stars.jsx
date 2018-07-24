9118380dbcefefb25230589e140a8b1115e05715
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-07-24 12:27:46 -0700

    Fix Mock Data to generate Ratings

--- client/src/components/Stars.jsx
+++ client/src/components/Stars.jsx
@@ -10,12 +10,55 @@ class Stars extends React.Component {
 	}
 
 	render () {
-		return (
-			<div className="search">
-				{this.props.rating} stars
-				
-			</div>
-		)
+		if (this.props.rating !== 6) {
+			return (
+				<div className="search">
+					<img src="star.gif" alt="Star"/>
+				</div>
+			)
+		} else if (this.props.rating === 0) {
+			return (
+				<div className="search">
+					<img src="star.gif" alt="Star"/>
+				</div>
+			)
+		} else if (this.props.rating === 1) {
+			return (
+				<div className="search">
+					<img src="star.gif" alt="Star"/>
+				</div>
+			)
+		}else if (this.props.rating === 2) {
+			return (
+				<div className="search">
+					<img src="star.gif" alt="Star"/>
+				</div>
+			)
+		} else if (this.props.rating === 3) {
+			return (
+				<div className="search">
+					<img src="star.gif" alt="Star"/>
+				</div>
+			)
+		} else if (this.props.rating === 4) {
+			return (
+				<div className="search">
+					<img src="star.gif" alt="Star"/>
+				</div>
+			)
+		} else if (this.props.rating === 5) {
+			return (
+				<div className="search">
+					<img src="star.gif" alt="Star"/>
+				</div>
+			)	
+		} else {	
+			return (
+					<div className="search">
+						<img src="star.gif" alt="no stars"/>
+					</div>
+				)
+		}
 	}
 }
 
