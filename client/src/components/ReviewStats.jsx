1c03ee5c8d76281aaf159340a357b4a09d102f72
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-07-24 16:58:21 -0700

    Add CSS Grid to Reviews Ratings

--- client/src/components/ReviewStats.jsx
+++ client/src/components/ReviewStats.jsx
@@ -27,16 +27,22 @@ class ReviewStats extends React.Component {
 
 	render () {
 		return (
-			<div className="reviewstats">
+			<div className={styles.wrapper}>
 				<div className="leftcolreviews">
-					<div> Accuracy <Stars rating={this.state.accuracy}/> </div> 
-					<div> Communication <Stars rating={this.state.communication}/></div> 
-					<div> Cleanliness <Stars rating={this.state.cleanliness}/></div> 
+					<div className={styles.label1}> Accuracy </div>
+					<div className={styles.stars1}><Stars rating={this.state.accuracy}/> </div> 
+					<div className={styles.label2}> Communication </div>
+					<div className={styles.stars2}><Stars rating={this.state.communication}/></div> 
+					<div className={styles.label3}> Cleanliness </div>
+					<div className={styles.stars3}><Stars rating={this.state.cleanliness}/></div> 
 				</div>
 				<div className="rightcolreviews">
-					<div> Location <Stars rating={this.state.location}/></div> 
-					<div> Check-in <Stars rating={this.state.checkin}/></div> 
-					<div> Value <Stars rating={this.state.value}/></div> 
+					<div className={styles.label4}> Location </div>
+					<div className={styles.stars4}><Stars rating={this.state.location}/></div> 
+					<div className={styles.label5}> Check-in </div>
+					<div className={styles.stars5}><Stars rating={this.state.checkin}/></div> 
+					<div className={styles.label6}> Value </div>
+					<div className={styles.stars6}> <Stars rating={this.state.value}/></div> 
 				</div>
 			</div>
 		)
