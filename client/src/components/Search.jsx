3fbf76b05b49832d4b0321d279f71fc6c3060eb2
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-07-25 10:49:14 -0700

    Change components to stateless

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,41 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import styles from './SearchStyles.css';
 
-class Search extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			placeholder: "Search Reviews",
-		}
-		this.clearSearch = this.clearSearch.bind(this);
-	}
-	
-	clearSearch () {
-		this.setState({
-			placeholder: "Search Reviews",
-		});
-	}
-
-	render () {
-		return (
-			<div className={styles.searchContainer}>
-				<i className={styles.searchIcon} ></i>
-				<input 
-					type="search"
-					className={styles.searchBox}
-					placeholder={this.state.placeholder} 
-					onChange={this.props.handleChange} 
-					onKeyPress={(event) => {
-						if (event.key === 'Enter') {
-						  this.props.searchReviews();
-						}
-					}}
-				/>
-				<div className={styles.clearSearch} onClick={() => this.clearSearch()}> X </div>
-			</div>
-		)
-	}
+const Search = (props) => {
+	return (
+		<div className={styles.searchContainer}>
+			<i className={styles.searchIcon} ></i>
+			<input 
+				type="search"
+				className={styles.searchBox}
+				placeholder="Search Reviews" 
+				onChange={props.handleChange} 
+				onKeyPress={(event) => {
+					if (event.key === 'Enter') {
+						props.searchReviews();
+					}
+				}}
+			/>
+		</div>
+	);
 }
 
 export default Search;
