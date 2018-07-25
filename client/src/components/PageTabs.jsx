f2dc66a731f27c3b51f62500f9d2496338545ffb
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-07-25 16:34:15 -0700

    Start on PageTabs

--- client/src/components/PageTabs.jsx
+++ client/src/components/PageTabs.jsx
@@ -6,14 +6,25 @@ class PageTabs extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			
+			totalTabs: 1,
 		}
+	}
+
+	calculateTabs () {
+		let totalTabs = Math.ceil(this.props.totalReviews/this.props.pageNum);
+		this.setState({
+			totalTabs: totalTabs,
+		})
+	}
+
+	componentDidMount () {
+		this.calculateTabs();
 	}
 
 	render () {
 		return (
 			<div className="pagetabs">
-				PageTabs will go here
+				Imagine {this.state.totalTabs} tabs are here
 			</div>
 		)
 	}
