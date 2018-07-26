f153e3a506e7da520ba259955c58deea59a9b029
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-07-26 12:31:34 -0700

    Edit Header Styling

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -6,19 +6,23 @@ import Search from './Search.jsx';
 const Header = (props) => {
 		return (
 			<div className={styles.wrapper}>
-				<div className={styles.total}>
-					{props.reviews.length} Reviews 
+				<div className={styles.leftcol}>
+					<div className={styles.total}>
+						{props.reviews.length} Reviews 
+					</div>
+					<div className={styles.starsRating}> 
+						<Stars 
+							rating={props.totalRating}
+						/> 
+					</div>
 				</div>
-				<div className={styles.col2}> 
-					<Stars 
-						rating={props.totalRating}
-					/> 
-				</div>
-				<div className={styles.col3}>
-					<Search
-						searchReviews={props.searchReviews}
-						handleChange={props.handleChange}
-					/>
+				<div className={styles.rightcol}>
+					<div className={styles.search}>
+						<Search
+							searchReviews={props.searchReviews}
+							handleChange={props.handleChange}
+						/>
+					</div>
 				</div>
 			</div>
 		);
