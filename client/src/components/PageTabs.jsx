423592ad461da7afa4d2c4d257417e4eb18bb8d6
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-07-25 19:40:59 -0700

    Add Functioning PageTabs

--- client/src/components/PageTabs.jsx
+++ client/src/components/PageTabs.jsx
@@ -9,6 +9,8 @@ class PageTabs extends React.Component {
 			totalTabs: 1,
 		};
 		this.displayTab = this.displayTab.bind(this);
+		this.displayAllTabs = this.displayAllTabs.bind(this);
+		
 	}
 
 	componentDidMount () {
@@ -16,6 +18,13 @@ class PageTabs extends React.Component {
 	}
 
 	displayTab (tabNum) {
+		if (tabNum === this.props.pageNum) {
+			return (
+				<div className={styles.currenttab}>
+					<span onClick={this.props.changePage}> {tabNum} </span>
+				</div>
+			);
+		}
 		return (
 			<div className={styles.tab}>
 				<span onClick={this.props.changePage}> {tabNum} </span>
@@ -23,15 +32,62 @@ class PageTabs extends React.Component {
 		);
 	}
 
+	displayAllTabs () {
+		if (this.props.pageNum === 1){
+			return (
+				<div className={styles.pageTabs}>
+				{this.displayTab(this.props.pageNum)}
+				{this.displayTab(this.props.pageNum + 1)}
+				<div className={styles.tab}> ... </div>
+				{this.displayTab(this.props.totalTabs)}
+				</div>
+			);
+		} else if (this.props.pageNum === 2){
+				return (
+					<div className={styles.pageTabs}>
+					{this.displayTab(1)}
+					{this.displayTab(this.props.pageNum)}
+					{this.displayTab(this.props.pageNum + 1)}
+					<div className={styles.tab}> ... </div>
+					{this.displayTab(this.props.totalTabs)}
+					</div>
+				);
+
+		} else if (this.props.pageNum === this.props.totalTabs) {
+			return (
+				<div className={styles.pageTabs}>
+				{this.displayTab(1)}
+				<div className={styles.tab}> ... </div>
+				{this.displayTab(this.props.pageNum - 1)}
+				{this.displayTab(this.props.pageNum)}
+				</div>
+			);
+			
+		} else {
+			return (
+				<div className={styles.pageTabs}>
+				{this.displayTab(1)} 
+				<div className={styles.tab}> ... </div>
+				{this.displayTab(this.props.pageNum - 1)}
+				{this.displayTab(this.props.pageNum)}
+				{this.displayTab(this.props.pageNum + 1)}
+				<div className={styles.tab}> ... </div>
+				{this.displayTab(this.props.totalTabs)}
+				</div>
+			);
+		}
+	}
+
 	render () {
 		console.log(this.props.pageNum);
 		console.log(this.props.totalTabs);
 
 		return (
-			<div className="pagetabs">
+			<div>
 				Imagine {this.props.totalTabs} tabs are here
-				{this.displayTab(2)}
-				{this.displayTab(this.props.totalTabs)}
+				<div>
+					{this.displayAllTabs()}
+				</div>
 			</div>
 		)
 	}
