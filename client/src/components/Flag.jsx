b5769a4252bee2b98931c323238ab12eade315b5
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-07-27 22:55:55 -0700

    Add underline when hovering over learn more

--- client/src/components/Flag.jsx
+++ client/src/components/Flag.jsx
@@ -16,38 +16,39 @@ class Flag extends React.Component {
         <div className={styles.flag}>
 					<div className={styles.background}>
           </div>
-          <div className={styles.modal}>
-            <button className={styles.close} onClick={this.props.closeFlag}> X </button>
-            <header>
-              Do you want to anonymously report this review?
-            </header>
-            <div className={styles.directions}>
-              If so, please choose one of the following reasons. <span className={styles.learnMore}>Learn more</span>
+            <div className={styles.modal}>
+              <button className={styles} onClick={this.props.closeFlag}> X </button>
+              <header>
+                Do you want to anonymously report this review?
+              </header>
+              <div className={styles.directions}>
+                If so, please choose one of the following reasons. <span className={styles.learnMore}>Learn more</span>
+              </div>
+              <div>
+              <form className={styles.form}>
+                <div className={styles.option}> 
+                  <input className={styles.radio} type="radio" name="gender" value="inappropriate" /> 
+                  <div className={styles.title}>Inappropriate content </div>
+                  <span className={styles.text}>This review contains violent, graphic, promotional, or otherwise offensive content.</span>
+                  
+                </div>
+                <div className={styles.option}> 
+                  <input className={styles.radio} type="radio" name="gender" value="dishonest"/> 
+                  <div className={styles.title}>Dishonest or hateful content</div>
+                  <span className={styles.text}>This review is purposefully malicious and assaulting.</span>
+                  
+                </div>
+                <div className={styles.option}> 
+                  <input className={styles.radio} type="radio" name="gender" value="fake"/> 
+                  <div className={styles.title}> Fake content </div>
+                  <span className={styles.text}>This review contains false information or may be fake.</span>
+                </div>
+              </form>
+              </div>
+              <div className={styles.submit}> Submit </div>
             </div>
-            <div>
-            <form className={styles.form}>
-              <div className={styles.option}> 
-                <input className={styles.radio} type="radio" name="gender" value="inappropriate" /> 
-                <div className={styles.title}>Inappropriate content </div>
-                <span className={styles.text}>This review contains violent, graphic, promotional, or otherwise offensive content.</span>
-                
-              </div>
-              <div className={styles.option}> 
-                <input className={styles.radio} type="radio" name="gender" value="dishonest"/> 
-                <div className={styles.title}>Dishonest or hateful content</div>
-                <span className={styles.text}>This review is purposefully malicious and assaulting.</span>
-                
-              </div>
-              <div className={styles.option}> 
-                <input className={styles.radio} type="radio" name="gender" value="fake"/> 
-                <div className={styles.title}> Fake content </div>
-                <span className={styles.text}>This review contains false information or may be fake.</span>
-              </div>
-            </form>
-            </div>
-            <div className={styles.submit}> Submit </div>
           </div>
-				</div>
+        
       )
 		} else {
 			return (
