885548e5354372024c1839e613cedb9cf87a6807
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-07-30 16:09:08 -0700

    Implement suggested Changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -40,8 +40,8 @@ class App extends React.Component {
 			totalRating: 5,
 			searchResults: [],
 			showSearch: false,
-      searchTerm: null,
-      showPopUp: false,
+			searchTerm: null,
+			showPopUp: false,
 
 		}
 		this.searchReviews = this.searchReviews.bind(this);
