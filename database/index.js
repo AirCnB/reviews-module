b01b6e705c376406f682a26cbfb64ca132377d7a
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-08-03 14:42:34 -0700

    Change background flag to work with other components

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://database/review');
+mongoose.connect('mongodb://localhost/review');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', () => {
